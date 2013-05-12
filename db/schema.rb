24f7555dc37b5d64026462faf86917c7fb6f0279
Ariel Chait <apchait@gmail.com>
2013-05-12 17:54:06 -0500

    view changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130306064453) do
+ActiveRecord::Schema.define(:version => 20130424214406) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -45,6 +45,11 @@ ActiveRecord::Schema.define(:version => 20130306064453) do
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+
+  create_table "certifications_contracts", :force => true do |t|
+    t.integer "contract_id"
+    t.integer "certification_id"
+  end
 
   create_table "documents", :force => true do |t|
     t.integer  "language_id"
