73da4639b6ffefee295c0daa44bdae50221168e6
Ariel Chait <apchait@gmail.com>
2013-05-13 16:19:30 -0500

    breadcumb file

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,8 +2,9 @@ class Word < ActiveRecord::Base
   attr_accessible :audio, :description, :image, :language_id, :pdf, :subtheme_id, :text_english, :text_image, :text_original
 
   validates :text_english, :uniqueness => true
-  belongs_to :language
   belongs_to :subtheme
+  has_one :theme, :through => :subtheme
+  has_one :language, :through => :subtheme
 
   has_many :documents
 end
